c45329a0397f0885b80a156100fd05eed39a4db4
Hernavch <hernandezvc1292@gmail.com>
2019-03-03 13:29:36 -0800

    Songs and albums

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -15,6 +15,22 @@ spotify.search({ type: 'track', query: term }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-   
-  console.log(data.tracks.items[0]); 
+
+  if (search === 'albums'){
+    for(var i = 0; i < 3; i++){
+      var albums= data.tracks.items[i].album.name; 
+      var artists= data.tracks.items[i].artists[0].name;
+      console.log("ALBUM: ", albums)
+      console.log("ARTIST: ", artists)   
+   }
+  }
+  if (search === 'songs'){
+    for(var j= 0; j<5; j++){
+    var songs= data.tracks.items[j].name;
+    var artist=data.tracks.items[j].artists[0].name; 
+    console.log("SONG NAME: ",songs);
+    console.log("Artist: ",artist);
+    }
+  }
+  
   });
